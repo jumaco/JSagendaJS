96e32732bb346667e3d35191d1152c616a919d61
Juan Corral <juan.manuel.corral@hotmail.com>
2021-06-24 20:35:48 -0300

    Puliendo el buscador con include, para buscar y luego renderizar una lista sin repeticiones

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ if ((JSON.parse(localStorage.getItem('listaContactos'))) != null){
         console.log(JSON.parse(localStorage.getItem('listaContactos')))
         // console.table(JSON.parse(localStorage.getItem('listaContactos')))
         var listaContactos = JSON.parse(localStorage.getItem('listaContactos'));
-
+// A partir de la lista encontrada o creada muestra en DOM
         render(listaContactos)
         badge()
 }else{
@@ -23,8 +23,27 @@ function badge(){
     nroContac.innerHTML = cuenta;
 }
 
-function ordenar(array){
-    array.sort((a, b) => {
+
+
+
+function filtrarDuplicado(arr){
+// si el array contiene objetos iguales lo omite en un nuevo array
+    let resultado = [];
+    arr.forEach((item)=>{
+        //resultado incluye ya al itmem? si es true pushea el item, pero al estar negado pushea cuando no lo incluye
+        if(!resultado.includes(item)){
+            resultado.push(item);
+        }
+    })
+    // console.log(resultado);
+    return resultado
+};
+
+
+
+
+function ordenar(arr){
+    arr.sort((a, b) => {
         if(a.apellido.toLowerCase() < b.apellido.toLowerCase()) {
             return -1;
         }
@@ -41,14 +60,11 @@ function ordenar(array){
     })
 }
 
-
-// A partir de la lista encontrada o creada muestra en DOM
-
-function render(arrayVer){
+function render(arr){
         // const listado = document.getElementById('listado')
-        ordenar(arrayVer)
+        ordenar(arr)
         let i=0
-        arrayVer.forEach(list => {
+        arr.forEach(list => {
             i++
         let card = document.createElement('article')
         card.innerHTML = `
@@ -76,46 +92,13 @@ function render(arrayVer){
     })
 };
 
-// function render(){
-//         // let listado = document.getElementById('listado')
-//         let i=0
-//         ordenar(listaContactos)
-//     // con la lista ya ordenada pasa al for each para renderizar cada objeto
-//         listaContactos.forEach(list => {
-//             i++
-//         let card = document.createElement('article')
-//         card.innerHTML = `
-//                         <div class="card">
-//                             <div class="card-header" id="heading${i}">
-//                                 <h5 class="mb-0">
-//                                     <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
-//                                         <h4>${list.apellido}, ${list.nombre}</h4>
-//                                     </button>
-//                                 </h5>
-//                             </div>
-//                             <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#listado">
-//                                 <div class="card-body">
-//                                     <p>Teléfono: ${list.telefono}</p>
-//                                     <p>Email: ${list.mail}</p>
-//                                 </div>
-//                                 <div>
-//                                     <button type="button" class="btn btn-primary btn-sm edit${i}">Editar</button>
-//                                     <button type="button" class="btn btn-danger btn-sm remov${i}">Eliminar</button>
-//                                 </div>
-//                             </div>
-//                         </div>
-//         `
-//         listado.appendChild(card)
-//     })
-
-// }
 
 function clear (){
     // let element = document.getElementById("listado");
     while (listado.firstChild) {
     listado.removeChild(listado.firstChild);
     }
-}
+};
 
 // Crea nuevo contacto a partir de los inputs, crea un objeto "Contacto" y luego lo pushea a la "listaContactos" inicial.
 // Limpia los inputs y luego remueve DOM creado para volver a ejecutar "render".
@@ -133,42 +116,61 @@ function agregar(){
             this.mail = mail;
         }
         confirmarContacto(){
-            listaContactos.push(nuevoContacto);
-            localStorage.setItem ('listaContactos', JSON.stringify(listaContactos))
+            // if(!listaContactos.includes(nuevoContacto)){
+            //     console.log(nuevoContacto)
+            //     console.log(listaContactos.includes(nuevoContacto))
+            //     alert("Contacto Existente")
+
+
+
+            // }else{
+
+                console.log(listaContactos.includes(nuevoContacto))
+                
+                console.log(nuevoContacto)
+                listaContactos.push(nuevoContacto);
+                // listaContactos.push(nuevoContacto);
+                localStorage.setItem ('listaContactos', JSON.stringify(listaContactos))
                 console.log(JSON.parse(localStorage.getItem('listaContactos')))
-            document.getElementById("Input1").value="";
-            document.getElementById("Input2").value="";
-            document.getElementById("Input3").value="";
-            document.getElementById("Input4").value="";
-            clear()
-            render(listaContactos)
-            }
+                document.getElementById("Input1").value="";
+                document.getElementById("Input2").value="";
+                document.getElementById("Input3").value="";
+                document.getElementById("Input4").value="";
+                clear()
+                render(listaContactos)
+
+            // }
+
+        }
     }
     var nuevoContacto = new Contacto(nombre, apellido, telefono, mail);
     nuevoContacto.confirmarContacto();
     console.log(listaContactos.length);
     badge()
-}
+};
 
-var dataI = document.getElementById("idBuscar");
-dataI.onkeyup = () => {buscar()};
-dataI.onfocus = () => {document.getElementById("idBuscar").value=""};
+
+
+// var dataI = document.getElementById("idBuscar");
+idBuscar.onkeyup = () => {buscar()};
+idBuscar.onfocus = () => {idBuscar.value=""};
 
 function buscar(){
     const data = document.getElementById("idBuscar").value;
     var listaEncontrados = [];
     clear()
-
     for (const list of listaContactos) {
         for (const valor in list){
-            if ((list[valor]) === data) {
-                console.log(list)
+            if ((list[valor].toLowerCase()).includes(data.toLowerCase())) {
+                // console.log(list)
                 listaEncontrados.push(list);
-                console.log(listaEncontrados)
+                // console.log(listaEncontrados)
             }
         }
     }
-    render(listaEncontrados)
+    let busqueda = filtrarDuplicado(listaEncontrados);
+    // console.log(busqueda)
+    render(busqueda);
 };
 
 function mostrasLista() {
