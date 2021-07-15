a404b4569bc8607f800dc23e4482e62c5a54ee97
Juan Corral <juan.manuel.corral@hotmail.com>
2021-07-15 00:07:05 -0300

    agregando validaciones al 'agregar contacto'

--- js/api_script.js
+++ js/api_script.js
@@ -2,94 +2,137 @@ $("document").ready(function($){
 
 const APIURL = "https://api.openweathermap.org/data/2.5/onecall?lat=-34.84&lon=-58.37&units=metric&appid=7f0a3742fd58ecbb39f4104f191a5716"; 
 
-
+var current = {}
+var minutely = []
+var hourly = []
+var daily = []
+var alerts = []
 
 if ((JSON.parse(localStorage.getItem('rtaAPI'))) != null){
-        console.log("API Almacenados en LocalStorage")
-        console.log(JSON.parse(localStorage.getItem('rtaAPI')))
-        var misDatos = JSON.parse(localStorage.getItem('rtaAPI'));
-        console.log("API Almacenados en memoria")
-        console.log(misDatos);
-}else{
-    var misDatos = {};
-    obtenerAPI()
-};
+		// console.log("API Almacenados en LocalStorage")
+		// console.log(JSON.parse(localStorage.getItem('rtaAPI')))
+		var misDatos = JSON.parse(localStorage.getItem('rtaAPI'));
+		// console.log("API Almacenados en memoria")
+		// console.log(misDatos);
+		// console.log(misDatos.current.dt*1000)
+		// console.log(Date.now())
+		if (!(misDatos.current.dt*1000 < Date.now()+600000)) {
+			// console.log("ok, voy a usar solicitar a la API")
+			obtenerAPI();
+		}else{
+		// console.log("bueno, continuo con el localStorage")
+		current = misDatos.current
+		minutely = misDatos.minutely
+		hourly = misDatos.hourly
+		daily = misDatos.daily
+		alerts = misDatos.alerts
+		renderCurrent()
 
+		badge(`CLIMA T ${current.temp}º ST ${current.feels_like}º`,temp)
+		}
+}else{
+	var misDatos = {};
+	obtenerAPI()
+};
 
 function obtenerAPI(){
-    $.get(APIURL, function (respuesta, estado){
-    console.log(estado)
-    console.log(respuesta)
-    if(estado === "success"){
-        misDatos = respuesta;
-        localStorage.setItem('rtaAPI', JSON.stringify(misDatos));
-        }
-    });
+	console.log('obteniendo')
+	$.get(APIURL, function (respuesta, estado){
+	console.log(estado)
+	console.log(respuesta)
+	if(estado === "success"){
+		misDatos = respuesta;
+		localStorage.setItem('rtaAPI', JSON.stringify(misDatos));
+		current = misDatos.current
+		minutely = misDatos.minutely
+		hourly = misDatos.hourly
+		daily = misDatos.daily
+		alerts = misDatos.alerts
+
+		// console.log("variables a partir de misDatos")
+		// console.log(current)
+		// console.log(minutely)
+		// console.log(hourly)
+		// console.log(daily)
+		console.log(alerts)
+		renderCurrent()
+
+		badge(`CLIMA ${current.temp}º ST ${current.feels_like}º`,temp)
+		}
+	});
 };
 
-//Agregamos un botón con jQuery
-// $("#clima").prepend('<button id="btn1" class="btn btn-primary btn-sm">Obtener</button>');
-//Escuchamos el evento click del botón agregado
-// $("#btn1").click(obtenerApi());
 
-const current = misDatos.current
-const minutely = misDatos.minutely
-const hourly = misDatos.hourly
-const daily = misDatos.daily
-const alerts = misDatos.alerts
 
-console.log("variables a partir de misDatos")
-console.log(current)
-console.log(minutely)
-console.log(hourly)
-console.log(daily)
-console.log(alerts)
 
-badge(`CLIMA <span class="badge">${current.temp}º ST ${current.feels_like}º"</span>`,temp)
-renderCurrent()
 
 function renderCurrent(){
-    let card = document.createElement('article')
-    let dateActualizacion = new Date(current.dt*1000);
-    card.innerHTML =`
-                    <div class="card text-center">
-                        <div class="card-header">
-                            ${misDatos.timezone}
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Temperatura: ${current.temp}ºC / Sensación Térmica: ${current.feels_like}ºC</h5>
-                            <p class="card-text">${alerts[0].description}</p>
-                            <a id="actualizar" href="#" class="btn btn-primary">Actualizar</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                            ${dateActualizacion}
-                        </div>
-                    </div>
-                    `
-    clima.appendChild(card)
+	let card = document.createElement('article')
+	let dateActualizacion = new Date(current.dt*1000);
+	card.innerHTML =`
+					<div class="card text-center">
+						<div class="card-header">
+							<p class="text-muted mb-0 pb-0">Temperatura actual para</p
+							<h1>${misDatos.timezone}</h1>
+							
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">T: ${current.temp}ºC / ST: ${current.feels_like}ºC</h5>
+							<p class="card-text"></p>
+							<a id="actualizar" href="/" class="btn btn-primary">Actualizar</a>
+						</div>
+						<div class="card-footer text-muted">
+							${dateActualizacion}
+						</div>
+					</div>
+					`
+	clima.appendChild(card)
 };
 
 
+		// console.log(daily)
 
-// function render(arr){
-//     ordenar(arr)
-//     arr.forEach(list => {
-//     let card = document.createElement('article')
-//     card.innerHTML =
-//                     `
-//                     <div class="card-group">
-//                         <div class="card">
-//                             <img src="..." class="card-img-top" alt="...">
-//                             <div class="card-body">
-//                             <h5 class="card-title">Card title</h5>
-//                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//                         </div>
-//                     </div>
-//                     `
-//     listado.appendChild(card)
-//     })
-// };
+		let carrousel = document.createElement('article')
+		carrousel.innerHTML =`
+							<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="3000">
+								<div id="carouselInner" class="carousel-inner">
+									
+								</div>
+								<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
+							</div>
+						`
+		clima.appendChild(carrousel)
 
 
+
+		daily.forEach(list => {
+			let card = document.createElement('div')
+			card.className = "carousel-item";
+			card.innerHTML =
+							`
+								<div class="card d-block ">
+									<div class="card-body text-center">
+										
+										<h6 class="card-subtitle mb-2 text-muted">${new Date(list.dt*1000)}</h6>
+										<h5 class="card-title">${list.temp.day}ºC</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+									</div>
+								</div>
+							`
+			carouselInner.appendChild(card);
+		
+		});
+
+		 $('#carouselInner div').first().addClass('active');
+
+$('#actualizar').click(obtenerAPI());
 });
+
--- js/script.js
+++ js/script.js
@@ -1,47 +1,18 @@
-// Comprueba LocalStorage si existen contactos previos y los imprime, si no crea una lista.
+// COMPRUEBA LOCALSTORAGE SI EXISTEN CONTACTOS PREVIOS Y LOS ALMACENA EN UNA VARIABLE, SI NO CREA UN LISTA NUEVA ARRAY.
 if ((JSON.parse(localStorage.getItem('listaContactos'))) != null){
-        console.log("Almacenados en LocalStorage")
-        console.log(JSON.parse(localStorage.getItem('listaContactos')))
-        // console.table(JSON.parse(localStorage.getItem('listaContactos')))
         var listaContactos = JSON.parse(localStorage.getItem('listaContactos'));
-    // // AGREGANDO UN ID
-    //     for (let i = 0; i < listaContactos.length; i++) {
-    //         listaContactos[i].id = i;
-    //             // .toString(); // pasado a string para que funcione en buscador x el toLowerCase.
-    //     }
-        agregarId(listaContactos)
-        console.log("Almacenados en memoria con nuevo ID")
-        console.log(listaContactos);
-        // alert()
-        //A PARTIR DE LISTA ENCONTRADA FILTRA REPETIDOS
-        const aMostrar = filtrarDuplicado(listaContactos)
+    //A PARTIR DE LISTA ENCONTRADA FILTRA REPETIDOS
+        let aMostrar = filtrarDuplicado(listaContactos);
     // A partir de la lista encontrada muestra en DOM
-        render(aMostrar)
-        badge(`CONTACTOS <span class="badge">${listaContactos.length}</span>`,nroContac);
+        render(aMostrar);
+        badge(`CONTACTOS ${listaContactos.length}`,nroContac);
 }else{
         var listaContactos = [];
 };
 
-function agregarId (arr){
-            for (let i = 0; i < arr.length; i++) {
-            arr[i].id = i;
-                // .toString(); // pasado a string para que funcione en buscador x el toLowerCase.
-        }
-}
-
-
-
-// ---------------
-console.log("Almacenados en memoria")
-console.log(listaContactos)
-// console.log("Almacenados en memoria"+ JSON.stringify(listaContactos))
-
-
-// MUESTRA u OCULTA UNA SECCION
+// MUESTRA U OCULTA UNA SECCIÓN.
 const bloques = $(".bloque");
 const enlaces = $("#navegacion").find("a");
-// console.log(enlaces)
-
 enlaces.click(function(e){
     e.preventDefault();
     let iden = $(this).attr("href");
@@ -50,34 +21,40 @@ enlaces.click(function(e){
     $(e.target.parentNode).addClass("alert-primary border-bottom-0").siblings().addClass("alert-secondary")
 })
 
-// Contador de contactos
-// const badge = document.getElementById('nroContac');
+// ------------------------EVENTOS------------------------
+
+// CAPTURA EVENTOS y EJECUTA
+idBuscar.onkeyup = () => {buscar()};
+idBuscar.onfocus = () => {idBuscar.value=""};
+
+// ------------------------FUNCIONES------------------------
+
+// A CADA OBJETO DEL ARRAY LE CREA UN NUEVO ID O SOBREESCRIBE PARA QUE COINCIDA CON EL INDEX DEL ARRAY.
+function agregarId (arr){
+    for (let i = 0; i < arr.length; i++) {
+    arr[i].id = i;
+    };
+};
+
+// AGREGA UN BADGE(el1) AL FINAL DEL ELEMENTO HTML ESPECIFICADO POR ID(el2)
 function badge(el1, el2){
-    // var cuenta =`CONTACTOS <span class="badge">${listaContactos.length}</span>`;
     var cuenta = el1
     el2.innerHTML = cuenta;
 }
 
-
-// Filtra duplicados dentro de un array.
-// -en la version inicial se podían guardar contactos identicos.
-// -al momento de hacer una busqueda puede traer duplicados.
-
+// FILTRA DUPLICADOS DENTRO DE UN ARRAY.
+// -AL MOMENTO DE HACER UNA BUSQUEDA PUEDE TRAER DUPLICADOS.
 function filtrarDuplicado(arr){
-// si el array contiene objetos iguales lo omite en un nuevo array
+// SI EL ARRAY CONTIENE OBJETOS IGUALES LO OMITE EN UN NUEVO ARRAY.
     let resultado = [];
     arr.forEach((item)=>{
-        //resultado incluye ya al itmem? si es true pushea el item, pero al estar negado pushea cuando no lo incluye
+//RESULTADO[] INCLUYE YA AL ITEM? SI ES TRUE PUSHEA EL ITEM, PERO AL ESTAR NEGADO PUSHEA CUANDO NO LO INCLUYE.
         if(!resultado.includes(item)){
             resultado.push(item);
         }
     })
-    console.log("Array sin repetidos")
-    console.log(resultado);
     return resultado
 };
-
-
 
 // ORDENA EL ARRAY SEGUN APELLIDO Y LUEGO POR NOMBRE
 function ordenar(arr){
@@ -98,14 +75,9 @@ function ordenar(arr){
     })
 }
 
-
-// RENDERIZA UN ARRAY 
-// SE OMITIÓ EL LA VARIABLE i, YA QUE AHORA UTILIZA EL id DENTRO DEL OBJETO PARA DISCRIMINAR.
-// OMITÍ EL getElementById YA QUE AL USAR UN id HTML JS lo reconoce ¿?.
+// RENDERIZA EL ARRAY (QUE SE ESPECIFICA EN EL PARAMETRO DE LA FUNCION).
 function render(arr){
-    // const listado = document.getElementById('listado').
     ordenar(arr)
-    // let i=0
     arr.forEach(list => {
     let card = document.createElement('article')
     card.innerHTML =
@@ -131,26 +103,23 @@ function render(arr){
                         </div>
                     `
     listado.appendChild(card)
-    // i++
     })
 };
 
-
+// LIMPIA EL DOM SEGÚN EL ID ESPECIFICADO COMO PARÁMETRO.
 function clear (id){
-    // let element = document.getElementById("listado");
     while (id.firstChild) {
     id.removeChild(id.firstChild);
     }
 };
 
-// Crea nuevo contacto a partir de los inputs, crea un objeto "Contacto" y luego lo pushea a la "listaContactos" inicial.
-// Limpia los inputs y luego remueve DOM creado para volver a ejecutar "render".
+// CREA NUEVO CONTACTO A PARTIR DE LOS INPUTS, CREA UN OBJETO "CONTACTO" Y LUEGO LO PUSHEA A LA "LISTACONTACTOS" INICIAL.
+// LIMPIA LOS INPUTS Y LUEGO REMUEVE DOM CREADO PARA VOLVER A EJECUTAR "RENDER" Y CREA UNA NOTIFICACIÓN QUE SE GUARDÓ.
 function agregar(){
     const nombre = $("#Input1").val();
     const apellido = $("#Input2").val();
     const telefono = $("#Input3").val();
     const mail = $("#Input4").val();
-
     class Contacto {
         constructor(nombre, apellido, telefono, mail) {
             this.nombre = nombre;
@@ -159,36 +128,30 @@ function agregar(){
             this.mail = mail;
         }
         confirmarContacto(){
-            console.log("el de abajo es el nuevo contacto")
-            console.log(nuevoContacto);
             agregarId(listaContactos);
             listaContactos.push(nuevoContacto);
             localStorage.setItem ('listaContactos', JSON.stringify(listaContactos));
-            console.log(JSON.parse(localStorage.getItem('listaContactos')));
-            document.getElementById("Input1").value="";
-            document.getElementById("Input2").value="";
-            document.getElementById("Input3").value="";
-            document.getElementById("Input4").value="";
+            $("#Input1").val('');
+            $("#Input2").val('');
+            $("#Input3").val('');
+            $("#Input4").val('');
             clear(listado);
             render(listaContactos);
-            $("#respuestaGuardado").prepend(`
-                <button type="button" class="btn btn-success">Contacto Guardado</button>
-            `);
+            $("#respuestaGuardado").prepend(`<button type="button" class="btn btn-success">Contacto Guardado</button>`);
             $(".btn-success").fadeOut(4000);
         }
+
     }
     var nuevoContacto = new Contacto(nombre, apellido, telefono, mail);
-    nuevoContacto.confirmarContacto();
-    console.log(listaContactos.length);
-    badge(`CONTACTOS <span class="badge">${listaContactos.length}</span>`,nroContac);
+
+
+    badge(`CONTACTOS ${listaContactos.length}`,nroContac);
 };
 
 
 
-// var dataI = document.getElementById("idBuscar");
-idBuscar.onkeyup = () => {buscar()};
-idBuscar.onfocus = () => {idBuscar.value=""};
-
+// BUSCADOR
+// DENTRO DE CADA ELEMENTO DE ARRAY(OBJETO) BUSCA LO INGRESADO EN EL VALOR DE LA KEY, LO ALMACENA EN UN NUEVO ARRAY, FILTRA REPETIDOS, RENDER AL NUEVO ARRAY
 function buscar(){
     const data = document.getElementById("idBuscar").value;
     var listaEncontrados = [];
@@ -197,57 +160,134 @@ function buscar(){
         for (const valor in list){
             if(valor != "id"){
                 if ((list[valor].toLowerCase()).includes(data.toLowerCase())) {
-                    // console.log(list)
                     listaEncontrados.push(list);
-                    console.log("lista de encontrados")
-                    console.log(listaEncontrados)
                 }
             }
         }
     }
     let busqueda = filtrarDuplicado(listaEncontrados);
-    // console.log(busqueda)
     render(busqueda);
 };
 
+// LA FUNCION ESTA INTEGRADA EN EL HTML AL PRESIONAR VER CONTACTOS, LIMPIA Y RENDERIZA LA LISTA
 function mostrasLista() {
     clear(listado);
     render(listaContactos);
 };
 
-
-// $(`#guardar`).on('click', function () {
-//     $("#respuestaGuardado").prepend(`
-//         <button type="button" class="btn btn-success">Contacto Guardado</button>
-//     `);
-//     $(".btn-success").fadeOut(4000)
-// });
-
-// ELIMINA CONTACTO MEDIANTE .splice A PARTIR DEL nuemero de index BORRA 1 OBJETO, EL ID DE CADA OBJETO SE HIZO COINCIDIR CON EL index DEL ARRAY.
-//LA FUNCION ES LLAMADA mediante onclick html JUNTO CON EL PARAMETRO QUE ES RELLENADO AL CREAR EL DOM
+// ELIMINA CONTACTO MEDIANTE .SPLICE A PARTIR DEL NÚMERO DE INDEX BORRA 1 OBJETO, EL ID DE CADA OBJETO SE HIZO COINCIDIR CON EL INDEX DEL ARRAY.
+//LA FUNCIÓN ES LLAMADA MEDIANTE ONCLICK HTML JUNTO CON EL PARÁMETRO QUE ES RELLENADO AL CREAR EL DOM.
 function eliminarContac (desdeIndex){
-        console.log("Al ejecutar eliminar Almacenados en LocalStorage antes del splice")
-        console.log(JSON.parse(localStorage.getItem('listaContactos')))
-        console.log("Al ejecutar eliminar Almacenados en memoria antes del splice")
-        console.log(listaContactos);
-        alert("con alert detengo que se actualice consola para ver el array antes de splice")
     listaContactos.splice(desdeIndex, 1); // Para la posición  1 elimina 1 elemento
-        console.log("Al ejecutar eliminar Almacenados en memoria luego del splice")
-        console.log(listaContactos);
-        alert("con alert detengo que se actualice consola para ver el array antes de reasignar id")
     agregarId(listaContactos);
     localStorage.setItem ('listaContactos', JSON.stringify(listaContactos))
-        console.log("Al ejecutar eliminar luego de splice y reasignar id Almacenados en memoria")
-        console.log(listaContactos);
-        console.log("Al ejecutar eliminar luego de splice y reasignar id Almacenados en LocalStorage")
-        console.log(JSON.parse(localStorage.getItem('listaContactos')))
     clear(listado)
-    badge(`CONTACTOS <span class="badge">${listaContactos.length}</span>`,nroContac);
+    badge(`CONTACTOS ${listaContactos.length}`,nroContac);
     render(listaContactos);
-    $("#respuestaGuardado").prepend(`
-        <button id="eliminado" type="button" class="btn btn-danger">Contacto Eliminado</button>
-    `);
-    $("#eliminado").fadeOut(4000)
+    $("#respuestaGuardado").prepend(`<button id="eliminado" type="button" class="btn btn-danger">Contacto Eliminado</button>`);
+    $("#eliminado").fadeOut(4000);
 }
+
+addContact.onclick= () => {
+    const labels = $("label");
+    const inputs = $("form").find("input");
+    console.log(inputs)
+    console.log(labels)
+
+    for (const input of inputs) {
+        console.log(input)
+        let valor = (input.$("input")).attr("id");
+        console.log(valor)
+
+
+    }
+}
+
+// inputs.blur(function(e){
+//     e.preventDefault();
+//     let iden = $(this).attr("id");
+//     labels.filter(iden).addClass("border border-danger");
+//     $(e.target.parentNode).removeClass("alert-primary alert-secondary border-bottom-0").siblings().removeClass("alert-primary alert-secondary border-bottom-0");
+//     $(e.target.parentNode).addClass("alert-primary border-bottom-0").siblings().addClass("alert-secondary")
+// })
+
+
+addContact.onclick= () => {
+    let reg = /^[A-Z0-9._%+-]+@([A-Z0-9-]+.)+[A-Z]{2,4}$/i;
+
+    console.log("corriendo validacion")
+    $('#Input1').blur(function() {
+        if($("#Input1").val().trim() == '' ){
+            $(this).addClass("border border-danger");
+        }else {
+            $(this).removeClass("border border-danger");
+            $('#guardar').removeAttr("disabled");
+        }
+    });
+    $('#Input2').blur(function() {
+        if($("#Input2").val().trim() == '' ){
+            $(this).addClass("border border-danger");
+            $('#guardar').attr("disabled","disabled");
+        }else {
+            $(this).removeClass("border border-danger");
+            $('#guardar').removeAttr("disabled");
+        }
+    });
+
+    $('#Input3').blur(function() {
+        console.log (parseInt($("#Input3").val().trim()))
+
+        if(($("#Input3").val().trim() == '') || (isNaN(parseInt($("#Input3").val().trim()))) ){
+            $(this).addClass("border border-danger");
+            $('#guardar').attr("disabled","disabled");
+        }else {
+            $(this).removeClass("border border-danger");
+            $('#guardar').removeAttr("disabled");
+        }
+        })
+
+    $('#Input4').blur(function() {
+        if($("#Input4").val().trim() == '' ){
+            $(this).addClass("border border-danger");
+            $('#guardar').attr("disabled","disabled");
+        }else {
+            $(this).removeClass("border border-danger");
+            $('#guardar').removeAttr("disabled");
+        }
+        })
+
+    $('#Input4').blur(function() {
+        mail = $("#Input4").val().trim()
+        if(mail != '' && reg.test(mail)){
+            $(this).removeClass("border border-danger");
+            $('#guardar').removeAttr("disabled");
+        }else {
+            $(this).addClass("border border-danger");
+            $('#guardar').attr("disabled","disabled");
+        }
+        })
+    if (($("#Input1").val() == '')||($("#Input2").val() == '')||($("#Input3").val() == '')||($("#Input4").val() == '')) {
+        $('#guardar').attr("disabled","disabled");
+    }
+}
+
+
+    // }else if(mail.trim() == '' ){
+    //     alert('Por favor, ingresa tu email.');
+    //     $('#Input4').focus();
+    //     return false;
+    // }else if(mail.trim() != '' && !reg.test(mail)){
+    //     alert('Por favor, ingresa un email válido.');
+    //     $('#Input4').focus();
+    //     return false;
+    // }else if(apellido.trim() == '' ){
+    //     alert('Por favor, ingresa un mensaje.');
+    //     $('#Input2').focus(); 
+    //     return false;
+    // }else{
+    //     $('#guardar').removeAttr("disabled");
+
+    // }
+
 
 
